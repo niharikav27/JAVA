45158ea873a14a787bde1c225873cacb806f598d
niharikav27 <neerjav80@gmail.com>
2026-02-24 22:21:18 +0530

    type conversion

---
+++ datatype.java
@@ -0,0 +1,41 @@
+class datatype{
+    public static void main(String[] args) {
+        // int a =3;
+        // byte b = (byte) a;
+        
+        // int a = 3;
+        // int b = 4;
+        // byte c = (byte) (a + b);
+        
+        // byte a = 3;
+        // int b = (int)a;
+        
+        // explicit casting(narowing)
+        // double x = 10.75;
+        // int y = (int) x;
+        
+        // float to int
+        // float f = 25.99f;
+        // int n = (int) f;
+        
+        // byte overflow
+        // byte b1 = 127;
+        // b1++;
+        
+        // large value in byte 256 -130
+        // byte (b2) = (byte) 130;
+        
+        // mixed data types
+        // int m = 10;
+        // double d = 5.5;
+        
+        // character to int
+        // char ch = 'A';
+        // int num =ch
+        
+        // int to char
+        int num2 = 66;
+        char ch2 = (char)num2;
+        System.out.println(ch2);
+    }
+}
