699d694109a2f03c22c1559daab42585466acb67
niharikav27 <neerjav80@gmail.com>
2026-02-24 22:05:04 +0530

    sum of digits

---
+++ main.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        int a = 123;
+        int sum = 0;
+        while (a>0){
+            sum = sum + (a%10);
+            a=a/10;
+        }
+        System.out.println(sum);
+    }
+}
