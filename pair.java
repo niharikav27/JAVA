1478eff32b75786ce1483314d5a86756f33fc0b6
niharikav27 <neerjav80@gmail.com>
2026-02-25 09:47:14 +0530

    pair index

---
+++ pair.java
@@ -0,0 +1,14 @@
+class pair{
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,5,0};
+        int target = 3;
+        for( int i = 0;i<arr.length-1;i++){
+            for(int j = i+1; j <arr.length ; j++){
+                if((arr[i]+arr[j])==target)
+    
+                System.out.println(i+ " "+j);
+            }
+        }
+        
+    }
+}
